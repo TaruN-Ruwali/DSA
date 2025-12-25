9a10aa58e2d9b2342f7ed9374b71cb27020a7186
ROGER <tarunruwali1610@gmail.com>
2025-12-25 23:55:57 +0530

    union pushed

--- STEP 2 - Array/EP18.cpp
+++ STEP 2 - Array/EP18.cpp
@@ -26,10 +26,11 @@ void rotate1(vector<int>& nums){
 
 }
 
-void rotate_k_left(vector<int>& nums, int k){//incomplete
+void rotate_k_left(vector<int>& nums, int k){
 
-    // Brute Force              time- O(3n)  space- O(n)
-
+    // Brute Force              time- O(3n=n)  space- O(n)
+    
+    k=k%nums.size(); 
     int temp[k];
     for (int i = 0; i < k; i++)
     {
@@ -43,6 +44,32 @@ void rotate_k_left(vector<int>& nums, int k){//incomplete
     {
         nums[nums.size()-k+i]=temp[i];
     }
+
+    // Optimal          time- O(n)      space-O(1)
+
+    /*
+    Use reversal algo.
+    reverse first k elements
+    reverse remaining elements(k to end)
+    at last reverse whole array(begin to end)
+    */
+    
+    int n=nums.size();
+    if (k<0)
+    {
+        cout<<"Invalid Rotation"<<endl;
+        return;
+    }
+    
+    k=k%n;
+    if (k==0)
+    {
+        return;
+    }
+    
+    reverse(nums.begin(),nums.begin()+k);
+    reverse(nums.begin()+k,nums.end());
+    reverse(nums.begin(),nums.end());
     
 }
 
@@ -89,7 +116,7 @@ void movezeros(vector<int>& nums){
     we will use 2 pointer approach.
     1st pointer will point towards the 0th index.
     2nd pointer will traverse and find the non zero elements .
-    adn when found one it will swap the 1st pointer with 2nd adn increase position of 1st.
+    and when found one it will swap the 1st pointer with 2nd and increase position of 1st.
     */
 
     int p1=-1;
@@ -107,8 +134,8 @@ void movezeros(vector<int>& nums){
     {
         if (nums[p2]!=0)
         {
-            nums[p1]=nums[p2];
-            nums[p2]=0;
+            swap(nums[p1],nums[p2]);
+            // nums[p2]=0;
             p1++;
         }
         
@@ -155,10 +182,10 @@ int main(){
     vector<int> nums1={1,2,3,4,5,6,7,8,9,10};
     vector<int> nums2={2,3,4,4,5,11,12};
     // rotate1(nums);
-    // rotate_k_left(nums,2);
+    // rotate_k_left(nums1,20001);
     // movezeros(nums);
     // cout<<Linear_search(nums,13);
-    unin(nums1,nums2);
+    // unin(nums1,nums2);
     for (int i = 0; i < nums1.size(); i++)
     {
         cout<<nums1[i]<<" ";
