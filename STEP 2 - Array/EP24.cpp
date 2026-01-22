ac701058a3e01661f2cdc4b4350c17046ede4bac
ROGER <tarunruwali1610@gmail.com>
2026-01-22 20:57:24 +0530

    Completed

---
+++ STEP 2 - Array/EP24.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+
+        // Brute        time-O(n^3)     space- O(1)
+        
+        // int n=nums.size();
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // int max=INT_MIN;;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         int sum=0;
+        //         for(int k=i;k<=j;k++){
+        //             sum+=nums[k];
+
+        //         }
+        //         if(sum > max){
+        //             max=sum;
+        //         }
+        //     }
+        // }
+        // return max;
+
+        // Better           time- O(n^2)    space- O(1) 
+
+        // int n=nums.size();
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // int max=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     int sum=0;
+        //     for(int j=i;j<n;j++){
+
+        //         sum+=nums[j];
+
+        //         if(sum > max){
+        //             max=sum;
+        //         }
+        //     }
+        // }
+        // return max;
+
+        // Optimal (Kadane's algo)
+
+        int max=INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum > max){
+                max=sum;
+            }
+            if(sum < 0){
+                sum=0;
+            }
+        }
+        return max;
+}
+
+int main(){
+    vector<int> nums = {2,0,2,1,1,0};
+    cout<<maxSubArray(nums)<<endl;
+    return 0;
+}
