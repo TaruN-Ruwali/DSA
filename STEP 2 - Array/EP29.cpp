523f5d9495163e91c975cdd55411f0df9788a20b
ROGER <tarunruwali1610@gmail.com>
2026-02-10 15:54:31 +0530

    EP29 Done

--- STEP 2 - Array/EP29.cpp
+++ STEP 2 - Array/EP29.cpp
@@ -1,7 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int longestConsecutive(vector<int>& nums) {
+
+            // Brute Approach       time-O(nlogn)       space- O(n)
+
+    //    sort(nums.begin(),nums.end());
+       
+    //    int n = nums.size();
+    //    if(n == 0)return 0;
+       
+    //    int count=1;
+    //    int max_count=1;
+       
+    //    for(int i=0;i<n-1;i++){
+    //         if(nums[i]+1 == nums[i+1]){
+    //             count++;
+    //         }
+    //         else if(nums[i] == nums[i+1]){
+    //             continue;
+    //         }
+    //         else{
+    //             count=1;
+    //         }
+    //         max_count = max(count,max_count);
+            
+    //    } 
+    //    return max_count;
+
+        // Optimal
+
+        int n = nums.size();
+        if(n==0)return 0;
+
+        unordered_set<int> st;
+        int max_count=1;
+
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int count=1;
+                int x=it;
+
+                while(st.find(x+1) != st.end()){
+                    count++;
+                    x++;
+                }
+                max_count = max(count , max_count);
+            }
+        }
+        return max_count;
+
+    }
+
 int main(){
-    
+    vector<int> a = {100, 4, 200, 1, 3, 2};
+
+
+    int ans = longestConsecutive(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
     return 0;
 }
