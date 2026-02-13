923195c00c1cd8f1ffcf4e1aee6312cf1452d914
ROGER <tarunruwali1610@gmail.com>
2026-02-13 14:27:35 +0530

    Better Approach

--- STEP 2 - Array/EP30.cpp
+++ STEP 2 - Array/EP30.cpp
@@ -33,6 +33,31 @@ void setZeroes(vector<vector<int>>& matrix) {
         //         }
         //     }
         // }
+
+                    // Better Approach      time- O(mn)     space- O(m+n)
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> rows(m);
+        vector<int> col(n);
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(rows[i] == 1 || col[j] == 1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
         
     }
 
