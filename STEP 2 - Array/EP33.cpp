e536fdfe83cbc271a0e8a11d160e18ec7bcdc054
ROGER <tarunruwali1610@gmail.com>
2026-03-03 10:34:26 +0530

    Optimal solution done

--- STEP 2 - Array/EP33.cpp
+++ STEP 2 - Array/EP33.cpp
@@ -23,15 +23,50 @@ int subarraySum(vector<int>& nums, int k) {
 
         // Better               time-O(n^2)     space-O(1)
 
+        // int n = nums.size();
+        // int sum=0,count=0;
+        // for(int i=0;i<n;i++){
+        //     sum=0;
+        //     for(int j=i;j<n;j++){
+        //         sum+=nums[j];
+        //         if(sum==k)count++;
+        //     }
+        // }
+        // return count;
+
+        // Optimal (Pre-fix)        time-O()       space-O(n)
+
         int n = nums.size();
-        int sum=0,count=0;
-        for(int i=0;i<n;i++){
-            sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k)count++;
+
+        // Map to store frequency of prefix sums
+        unordered_map<int, int> prefixSumCount;
+
+        // Initialize prefix sum and count of subarrays
+        int prefixSum = 0;
+        int count = 0;
+
+        // Base case: prefix sum 0 has occurred once
+        prefixSumCount[0] = 1;
+
+        // Traverse through the array
+        for (int i = 0; i < n; i++) {
+            // Add current element to prefix sum
+            prefixSum += nums[i];
+
+            // Calculate the prefix sum that needs to be removed
+            int remove = prefixSum - k;
+
+            // If this prefix sum has been seen before,
+            // add its count to the result
+            if (prefixSumCount.find(remove) != prefixSumCount.end()) {
+                count += prefixSumCount[remove];
             }
+
+            // Update the frequency of the current prefix sum
+            prefixSumCount[prefixSum]++;
         }
+
+        // Return the total count of subarrays
         return count;
     }
 
